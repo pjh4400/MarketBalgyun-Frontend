1ef0a0f4c116262f11c50bc20267bbd307ad9d6e
Jaehee Park <vito822@naver.com>
2020-09-16 18:42:08 +0900

    상품 관련 임시 데이터 생성

---
+++ src/tempDB/ConsignProducts.js
@@ -0,0 +1,19 @@
+const ConsignProducts = [
+    {
+        name: '바닷가 그림',
+        quantity: 1,
+        retail_price: 30000,
+        hopeful_price: 40000,
+        consigner_phone: '3268',
+        account_number: '71029381983',
+        bank: '국민',
+        settle: '포인트',
+        consign_date: '2020-06-27',
+        expiration_date: '',
+        something: '발리 여행 중 구매한 상품',
+        place: '예술/취미',
+        max_discount: 20,
+    },
+]
+
+export default ConsignProducts;
---
+++ src/tempDB/SecondCategory.js
@@ -0,0 +1,90 @@
+const SecondCategory = [
+    {
+        second_category : '일반가전',
+        first_category : '가전',
+        ID : '',
+    },
+    {
+        second_category : '주방가전',
+        first_category : '가전',
+        ID : '',
+    },
+    {
+        second_category : '브랜드그릇',
+        first_category : '그릇',
+        ID : '',
+    },
+    {
+        second_category : '일반그릇',
+        first_category : '그릇',
+        ID : '', 
+    },
+    {
+        second_category : '아동원서',
+        first_category : '도서',
+        ID : '',
+    },
+    {
+        second_category : '아동한글',
+        first_category : '도서',
+        ID : '',
+    },
+    {
+        second_category : '일반원서',
+        first_category : '도서',
+        ID : '',
+    },
+    {
+        second_category : '일반한글',
+        first_category : '도서',
+        ID : '',
+    },
+    {
+        second_category : '기타잡화',
+        first_category : '생활잡화',
+        ID : '',
+    },
+    {
+        second_category : '주방잡화브랜드',
+        first_category : '생활잡화',
+        ID : '',
+    },
+    {
+        second_category : '주방잡화일반',
+        first_category : '생활잡화',
+        ID : '',
+    },
+    {
+        second_category : '유아동용품(장난감/문구류)',
+        first_category : '유아동',
+        ID : '',
+    },
+    {
+        second_category : '의류및패션잡화',
+        first_category : '유아동',
+        ID : '',
+    },  
+    {
+        second_category : '3900',
+        first_category : '의류',
+        ID : '',
+    },
+    {
+        second_category : '7900',
+        first_category : '의류',
+        ID : '',
+    },
+    {
+        second_category : '브랜드',
+        first_category : '의류',
+        ID : '',
+    },
+    {
+        second_category : '유니크&빈티지',
+        first_category : '의류',
+        ID : '',
+    },
+]
+
+export default SecondCategory;
+
