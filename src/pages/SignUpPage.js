1c815a314bc9a52965c4edeba729bd61f7a7223e
Jaehee Park <vito822@naver.com>
2020-09-19 04:08:28 +0900

    회원가입 POST 구현

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Container, Paper, Typography, Grid, Button, Avatar, TextField, FormControlLabel, Link, MenuItem} from "@material-ui/core"
 
@@ -7,19 +8,18 @@ import useStyles from './Style';
 
 const SignUpPage = () => {
     const [form, setForm] = useState({
-        name: "",
-        position: "직원",
-        password: "",
-        password2: "",
+        name: '',
+        level: '직원',
+        password: '',
+        password2: '',
     })
     const classes = useStyles();
-
 
     const onChangeHandler = (e) => {
         e.preventDefault();
         setForm({
             ...form,
-            [e.target.name] : e.target.type==='checkbox' ? e.target.checked : e.target.value,
+            [e.target.name] : e.target.value,
         });
     }
 
@@ -27,6 +27,17 @@ const SignUpPage = () => {
         e.preventDefault();
         if(form.password === form.password2){
             alert('정상적으로 등록되었습니다.');
+            axios.post('api/auth/sign-up',{
+                name: form.name,
+                password: form.password,
+                level: form.level,
+            })
+            .then( (response) => {
+                console.log(response);
+            })
+            .catch( (error) => {
+                console.log(error);
+            })
         } else {
             alert('비밀번호가 일치하지 않습니다.');
         }
@@ -56,7 +67,7 @@ const SignUpPage = () => {
                         </Grid>
 
                         <Grid item xs={12} sm={6}>
-                            <TextField select label="직책" variant="outlined" className={classes.menuitem} onChange={onChangeHandler}>
+                            <TextField select label="직책" name="level" defaultValue="직원" variant="outlined"  className={classes.menuitem} onChange={onChangeHandler}>
                                 <MenuItem value="직원" >직원</MenuItem>
                                 <MenuItem value="팀장">팀장</MenuItem>
                                 <MenuItem value="대표">대표</MenuItem>
